f26700b8661bdcf32533bde28431db4662ebab8a
Mikhail Yudin <fagci.nsk@gmail.com>
2021-09-12 16:50:59 +0700

    refactor

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='fapy',
-    version='0.0.9',
+    version='0.0.10',
     description='''fagci's python library''',
     url='https://github.com/fagci/fapy/',
 
